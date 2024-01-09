f2216e3e4de8b968a61217b14ad76190075f079c
Shubham0523 <shubhamarora2306@gmail.com>
2024-01-10 04:02:32 +0530

    about-update

--- components/home/About.js
+++ components/home/About.js
@@ -7,16 +7,16 @@ export default function About() {
       id="about"
     >
       <p className="text-4xl font-bold">About Us</p>
-      <div className="max-w-2xl mx-auto text-gray-500 text-xl text-center my-4">
+      <div className="text-gray-500 text-xl w-full md:w-8/12 text-center my-4 p-4">
         <p className="text-justify">
           Welcome to Raed India, a compassionate NGO committed to guiding
-          individuals through the intricate process of reclaiming their unclaimed
-          investments and dividends. With a focus on transparency, efficiency, and
-          empathy, we strive to uncover financial happiness for every individual
-          by providing expert assistance in navigating the complexities of asset
-          recovery. Join us on a journey where we bridge the gap between unclaimed
-          funds and rightful owners, fostering a community empowered by financial
-          well-being and prosperity.
+          individuals through the intricate process of reclaiming their
+          unclaimed investments and dividends. With a focus on transparency,
+          efficiency, and empathy, we strive to uncover financial happiness for
+          every individual by providing expert assistance in navigating the
+          complexities of asset recovery. Join us on a journey where we bridge
+          the gap between unclaimed funds and rightful owners, fostering a
+          community empowered by financial well-being and prosperity.
         </p>
       </div>
     </div>
